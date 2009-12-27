affb6015b6ebc6828eba8c5d81225d109592643e
sina.heshmati@gmail.com <sina.heshmati@gmail.com@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2009-12-27 02:18:32 +0000

    Issue 7 in progress: Extended Nick's work on the RegisterUserAccount session bean; this impl. uses the built-in IdentityManager in Seam, which also assigns roles (see UserRole) and permissions to users and objects (resp.)

--- trunk/salesmen/core/src/hot/be/vub/salesmen/session/RegisterUserAccountBean.java
+++ trunk/salesmen/core/src/hot/be/vub/salesmen/session/RegisterUserAccountBean.java
@@ -1,63 +1,134 @@
 package be.vub.salesmen.session;
 
+import static org.jboss.seam.ScopeType.CONVERSATION;
+
 import java.io.Serializable;
+
+import javax.ejb.Remove;
 import javax.persistence.EntityManager;
 
 import org.jboss.seam.annotations.Begin;
+import org.jboss.seam.annotations.Destroy;
 import org.jboss.seam.annotations.End;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.annotations.Out;
+import org.jboss.seam.annotations.Observer;
 import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.faces.FacesMessages;
+import org.jboss.seam.security.Identity;
+import org.jboss.seam.security.RunAsOperation;
+import org.jboss.seam.security.management.IdentityManager;
+import org.jboss.seam.security.management.JpaIdentityStore;
 
 import be.vub.salesmen.entity.User;
 import be.vub.salesmen.entity.UserAccount;
 
-import static org.jboss.seam.ScopeType.CONVERSATION;
-
-
 @Scope(CONVERSATION)
-@Name("RegisterUserAccount")
+@Name("registerUserAccount")
 public class RegisterUserAccountBean implements RegisterUserAccount, Serializable
 {
-	
-	private static final long serialVersionUID = -4349512217411197622L;
-	
-	   @In
-	   EntityManager entityManager;
+	private static final long serialVersionUID = -5946808103467846878L;
 
-	   @Out
-	   private UserAccount newAccount;
-	   
-	   
-	   
-	   @Begin()
-	   public void newRegistration()
-	   {
-	      if (newAccount == null)
-	      {
-	    	 newAccount = new UserAccount();
-	    	 newAccount.setUser(new User());
-	      }
-	   }
+	private User user;
+	private UserAccount newAccount;
+	private String username;
+	private String password;
+	private String passwordConfirmation;
+	private boolean passwordVerified;
+	
+	@In
+	EntityManager entityManager;
 
-	   
-	   
-	   @End()
-	   public String register()
-	   {
+	@In
+	Identity identity;
+	
+	@In
+	IdentityManager identityManager;
+	
+	@Begin
+	public void createUser()
+	{
+		user = new User();
+	}
+
+	public void verifyPassword()
+	{
+		passwordVerified = (passwordConfirmation != null && passwordConfirmation.equals(password));
+		if (!passwordVerified)
+		{
+			FacesMessages.instance().addToControl("confirmPassword", "Passwords don not match");
+		}
+	}
+	
+	@Observer(JpaIdentityStore.EVENT_USER_CREATED)
+	public void userAccountCreated(UserAccount account)
+	{
+		account.setUser(user);
+		this.newAccount = account;
+	}
+	
+	@End
+	public void createUserAccount()
+	{
+		entityManager.persist(user);
+		new RunAsOperation()
+		{
+			public void execute()
+			{
+				identityManager.createUser(username, password);
+				identityManager.grantRole(username, "user");
+			}
+		}.addRole("admin").run();
 		
-		   newAccount.getUser().setUsername(newAccount.getUsername());
-		   newAccount.getUser().setFirstName("Test");
-		   newAccount.getUser().setLastName("Test2");
-		   
-		   newAccount.setPasswordHash("test");
-		   newAccount.setPasswordSalt("test");
-
-		   entityManager.persist(newAccount.getUser());
-		   entityManager.persist(newAccount);
-		   return "success";
-
-	   }
-
+		newAccount.setUser(user);
+		newAccount = entityManager.merge(newAccount);
+		
+		// Automatically, sign the user in
+		identity.getCredentials().setUsername(username);
+		identity.getCredentials().setPassword(password);
+		identity.logout();
+	}
+	
+	public User getUser()
+	{
+		return user;
+	}
+	
+	public String getUsername()
+	{
+		return username;
+	}
+	
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+	   
+	public String getPassword()
+	{
+		return password;
+	}
+	   
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+	   
+	public String getPasswordConfirmation()
+	{
+		return passwordConfirmation;
+	}
+	   
+	public void setPasswordConfirmation(String passwordConfirmation)
+	{
+		this.passwordConfirmation = passwordConfirmation;
+	}
+	   
+	public boolean isPasswordVerified()
+	{
+		return passwordVerified;
+	}
+	   
+	@Destroy @Remove
+	public void destroy() {}
 }
