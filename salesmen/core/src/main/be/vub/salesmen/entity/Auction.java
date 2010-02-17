4bbc11c21d729510e891edb89ef54256da2f0f79
datanuke@gmail.com <datanuke@gmail.com@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2010-02-17 09:00:08 +0000

    - regex pattern added for Auction.title

--- salesmen/core/src/main/be/vub/salesmen/entity/Auction.java
+++ salesmen/core/src/main/be/vub/salesmen/entity/Auction.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Version;
 import org.hibernate.validator.Length;
+import org.hibernate.validator.NotNull;
+import org.hibernate.validator.Pattern;
 
 @Entity
 public class Auction implements Serializable
@@ -34,7 +36,106 @@ public class Auction implements Serializable
         return id;
     }
 
-    public void setId(int id) {
+    /**
+	 * @return the owner
+	 */
+	public User getOwner() {
+		return owner;
+	}
+
+	/**
+	 * @param owner the owner to set
+	 */
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+
+	/**
+	 * @return the category
+	 */
+	public Category getCategory() {
+		return category;
+	}
+
+	/**
+	 * @param category the category to set
+	 */
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@NotNull
+    @Length(min=8, max=32)
+    @Pattern(regex="[a-zA-Z0-9,.:\t]+",
+    		message="Titles can only contain letters, numbers, ',', '.', ':' and spaces.")
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the endDate
+	 */
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	/**
+	 * @param endDate the endDate to set
+	 */
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	/**
+	 * @return the highBid
+	 */
+	public Bid getHighBid() {
+		return highBid;
+	}
+
+	/**
+	 * @param highBid the highBid to set
+	 */
+	public void setHighBid(Bid highBid) {
+		this.highBid = highBid;
+	}
+
+	/**
+	 * @return the startingPrice
+	 */
+	public double getStartingPrice() {
+		return startingPrice;
+	}
+
+	/**
+	 * @param startingPrice the startingPrice to set
+	 */
+	public void setStartingPrice(double startingPrice) {
+		this.startingPrice = startingPrice;
+	}
+
+	public void setId(int id) {
         this.id = id;
     }
 
