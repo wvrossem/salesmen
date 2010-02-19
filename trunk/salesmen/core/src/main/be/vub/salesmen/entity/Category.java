b37c86cafc192e070e17a317b2c18af9888e885f
sina.heshmati@gmail.com <sina.heshmati@gmail.com@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2010-02-19 18:27:37 +0000

    Added the parent attribute to the Category class and made it a foreign key in the persistence context.

--- trunk/salesmen/core/src/main/be/vub/salesmen/entity/Category.java
+++ trunk/salesmen/core/src/main/be/vub/salesmen/entity/Category.java
@@ -5,30 +5,29 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Version;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import org.hibernate.validator.Length;
+import org.jboss.seam.annotations.Name;
 
 @Entity
+@Name("category")
 public class Category implements Serializable
 {
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = -6813329677396178284L;
-	// seam-gen attributes (you should probably edit these)
+
     private Long id;
     private Integer version;
     private String name;
-
-    // add additional entity attributes
-
-    // seam-gen attribute getters/setters with annotations (you probably should edit)
+    private Category parent;
 
     @Id @GeneratedValue
-    public Long getId() {
+    public Long getCategoryId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setCategoryId(Long id) {
         this.id = id;
     }
 
@@ -36,8 +35,6 @@ public class Category implements Serializable
     public Integer getVersion() {
         return version;
     }
-
-
 
     @Length(max = 20)
     public String getName() {
@@ -48,4 +45,14 @@ public class Category implements Serializable
         this.name = name;
     }
 
+    @ManyToOne
+    @JoinColumn(name = "PARENT_CATEGORY_ID")    
+    public Category getParent() {
+        return parent;
+    }
+
+    public void setParent(Category parent)
+    {
+        this.parent = parent;
+    }
 }
