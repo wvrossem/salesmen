d73f900f1fb7c221768ee0c977ca6acd08e6227a
sina.heshmati@gmail.com <sina.heshmati@gmail.com@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2010-02-20 01:41:31 +0000

    Since my modifications were overwritten, I pushed them back in see r869 and r871

--- salesmen/core/src/main/be/vub/salesmen/entity/Category.java
+++ salesmen/core/src/main/be/vub/salesmen/entity/Category.java
@@ -5,23 +5,22 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Version;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import org.hibernate.validator.Length;
+import org.jboss.seam.annotations.Name;
 
 @Entity
+@Name("category")
 public class Category implements Serializable
 {
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = -6813329677396178284L;
-	// attributes (you should probably edit these)
+
     private Long id;
     private Integer version;
     private String name;
-
-    // add additional entity attributes
-
-    // attribute getters/setters with annotations (you probably should edit)
+    private Category parent;
 
     @Id @GeneratedValue
     public Long getId() {
@@ -37,11 +36,6 @@ public class Category implements Serializable
         return version;
     }
 
-    @SuppressWarnings("unused")
-	private void setVersion(Integer version) {
-        this.version = version;
-    }
-
     @Length(max = 20)
     public String getName() {
         return name;
@@ -51,4 +45,14 @@ public class Category implements Serializable
         this.name = name;
     }
 
+    @ManyToOne
+    @JoinColumn(name = "PARENT_CATEGORY_ID")    
+    public Category getParent() {
+        return parent;
+    }
+
+    public void setParent(Category parent)
+    {
+        this.parent = parent;
+    }
 }
