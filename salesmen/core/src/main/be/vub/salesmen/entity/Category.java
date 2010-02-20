c77616d4a1cc5368922c827681ff6f2b491f99a6
sina.heshmati@gmail.com <sina.heshmati@gmail.com@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2010-02-20 02:28:47 +0000

    My changes to Category.java seem to break the deployment so reverse-merged to rollback to Bart's revision

--- salesmen/core/src/main/be/vub/salesmen/entity/Category.java
+++ salesmen/core/src/main/be/vub/salesmen/entity/Category.java
@@ -5,22 +5,23 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Version;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
 import org.hibernate.validator.Length;
-import org.jboss.seam.annotations.Name;
 
 @Entity
-@Name("category")
 public class Category implements Serializable
 {
+    /**
+	 * 
+	 */
 	private static final long serialVersionUID = -6813329677396178284L;
-
+	// attributes (you should probably edit these)
     private Long id;
     private Integer version;
     private String name;
-    private Category parent;
+
+    // add additional entity attributes
+
+    // attribute getters/setters with annotations (you probably should edit)
 
     @Id @GeneratedValue
     public Long getId() {
@@ -36,6 +37,11 @@ public class Category implements Serializable
         return version;
     }
 
+    @SuppressWarnings("unused")
+	private void setVersion(Integer version) {
+        this.version = version;
+    }
+
     @Length(max = 20)
     public String getName() {
         return name;
@@ -45,14 +51,4 @@ public class Category implements Serializable
         this.name = name;
     }
 
-    @ManyToOne
-    @JoinColumn(name = "PARENT_CATEGORY_ID")    
-    public Category getParent() {
-        return parent;
-    }
-
-    public void setParent(Category parent)
-    {
-        this.parent = parent;
-    }
 }
