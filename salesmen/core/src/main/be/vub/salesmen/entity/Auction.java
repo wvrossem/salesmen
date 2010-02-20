e133d8138e4a08fcfa30a931ba86e13d269da09c
datanuke@gmail.com <datanuke@gmail.com@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2010-02-20 00:32:01 +0000

    - added basic Auction class (+ related files)

---
+++ salesmen/core/src/main/be/vub/salesmen/entity/Auction.java
@@ -0,0 +1,54 @@
+package be.vub.salesmen.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Version;
+import org.hibernate.validator.Length;
+
+@Entity
+public class Auction implements Serializable
+{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 2195670493386700385L;
+	//attributes (you should probably edit these)
+    private Long id;
+    private Integer version;
+    private String name;
+
+    // add additional entity attributes
+
+    //attribute getters/setters with annotations (you probably should edit)
+
+    @Id @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Version
+    public Integer getVersion() {
+        return version;
+    }
+
+    @SuppressWarnings("unused")
+	private void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    @Length(max = 20)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
