9a90fcfe3905e0a9dfdba9f5e26bad718062cd2c
datanuke@gmail.com <datanuke@gmail.com@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2010-02-20 00:29:52 +0000

    - added basic category class (+ related files)

---
+++ salesmen/core/src/hot/be/vub/salesmen/session/CategoryHome.java
@@ -0,0 +1,37 @@
+package be.vub.salesmen.session;
+
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Begin;
+import org.jboss.seam.annotations.web.RequestParameter;
+import org.jboss.seam.framework.EntityHome;
+
+import be.vub.salesmen.entity.Category;
+
+@Name("categoryHome")
+public class CategoryHome extends EntityHome<Category>
+{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -468312262064764801L;
+	@RequestParameter Long categoryId;
+
+    @Override
+    public Object getId()
+    {
+        if (categoryId == null)
+        {
+            return super.getId();
+        }
+        else
+        {
+            return categoryId;
+        }
+    }
+
+    @Override @Begin
+    public void create() {
+        super.create();
+    }
+
+}
