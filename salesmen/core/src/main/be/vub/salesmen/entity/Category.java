f73e4f06ea2273afd136a1594f765507ea99877a
datanuke@gmail.com <datanuke@gmail.com@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2010-02-20 12:37:46 +0000

    - added Category parent (not fully implemented yet)

--- salesmen/core/src/main/be/vub/salesmen/entity/Category.java
+++ salesmen/core/src/main/be/vub/salesmen/entity/Category.java
@@ -7,6 +7,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Version;
 import org.hibernate.validator.Length;
 
+/*
+ * Extra imports
+ * 
+ */
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 @Entity
 public class Category implements Serializable
 {
@@ -18,12 +25,23 @@ public class Category implements Serializable
     private Long id;
     private Integer version;
     private String name;
+    private Category parent;
 
     // add additional entity attributes
 
     // attribute getters/setters with annotations (you probably should edit)
 
-    @Id @GeneratedValue
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    public Category getParent() {
+		return parent;
+	}
+
+	public void setParent(Category parent) {
+		this.parent = parent;
+	}
+
+	@Id @GeneratedValue
     public Long getId() {
         return id;
     }
