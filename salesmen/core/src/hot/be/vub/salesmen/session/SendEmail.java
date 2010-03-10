99a22a858cc133a8c167c9d0072e6765f87e8968
jorne.laton@gmail.com <jorne.laton@gmail.com@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2010-03-10 22:04:40 +0000

    Committed changes to SendEmail class.

--- salesmen/core/src/hot/be/vub/salesmen/session/SendEmail.java
+++ salesmen/core/src/hot/be/vub/salesmen/session/SendEmail.java
@@ -5,9 +5,5 @@ import javax.ejb.Local;
 @Local
 public interface SendEmail
 {
-    // seam-gen method
-    public void sendEmail();
-
-    // add additional interface methods here
-    public void send(String emailxhtml);
+    public void send();
 }
