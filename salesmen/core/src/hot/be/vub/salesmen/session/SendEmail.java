4f552ab75e480ebe66f994eb4504b78bc8fc9858
jorne.laton@gmail.com <jorne.laton@gmail.com@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2010-03-20 16:27:31 +0000

    Added @In User to SendEmail.java.

--- salesmen/core/src/hot/be/vub/salesmen/session/SendEmail.java
+++ salesmen/core/src/hot/be/vub/salesmen/session/SendEmail.java
@@ -1,9 +1,42 @@
 package be.vub.salesmen.session;
 
-import javax.ejb.Local;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
 
-@Local
-public interface SendEmail
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.log.Log;
+import org.jboss.seam.faces.*;
+import be.vub.salesmen.entity.User;
+
+@Name("sendEmail")
+@Scope(ScopeType.CONVERSATION)
+public class SendEmail implements Serializable
 {
-    public void send();
+	private static final long serialVersionUID = -8132989898020136666L;
+
+	@Logger
+    private Log log;
+
+    @In
+    private FacesMessages facesMessages;
+    
+    @In
+    private Renderer renderer;
+	
+	@In(create=true)
+    private User user;
+
+    public void send() {
+       try {
+           renderer.render("/confirmEmail.xhtml");
+           facesMessages.add("Email sent successfully");
+       } catch (Exception e) {
+    	   log.error("Error sending mail", e);
+           facesMessages.add("Email sending failed: " + e.getMessage());
+       }
+    }
 }
