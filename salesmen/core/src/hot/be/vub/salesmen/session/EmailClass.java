fa8a6479ecbb9bd94a21feb73e4fdecdcebeee5c
jorne.laton@gmail.com <jorne.laton@gmail.com@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2010-03-24 15:28:21 +0000

    Created EmailClass to support different emails.

---
+++ salesmen/core/src/hot/be/vub/salesmen/session/EmailClass.java
@@ -0,0 +1,67 @@
+package be.vub.salesmen.session;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.Length;
+import org.hibernate.validator.NotNull;
+import org.jboss.seam.annotations.Name;
+
+@Name("emailClass")
+public class EmailClass implements Serializable {
+	
+	private static final long serialVersionUID = 4629653426165185678L;
+	
+	public EmailClass(String fullName, String emailAddress, String subject, String content) {
+		this.fullName = fullName;
+		this.emailAddress = emailAddress;
+		this.subject = subject;
+		this.content = content;
+	}
+	
+	public EmailClass() {}
+	
+	private String fullName;
+	private String emailAddress;
+	private String subject;
+	private String content;
+	
+	@NotNull
+	@Length(min=3, max=64)
+	public String getFullName() {
+		return fullName;
+	}
+	
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	@NotNull
+	@Length(min=3, max=32)
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+	@NotNull
+	@Length(min=3, max=64)
+	public String getSubject() {
+		return subject;
+	}
+	
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	@NotNull
+	@Length(min=3, max=1024)
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
