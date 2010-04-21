f2e4226f437e120c37a69791b022024b8a4daa10
jorne.laton@gmail.com <jorne.laton@gmail.com@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2010-04-21 14:55:51 +0000

    [code] Added methods addComment and updateComments, and attributes necessary for UserComment to ViewAuction.

--- trunk/salesmen/core/src/hot/be/vub/salesmen/session/ViewAuction.java
+++ trunk/salesmen/core/src/hot/be/vub/salesmen/session/ViewAuction.java
@@ -2,6 +2,7 @@ package be.vub.salesmen.session;
 
 import be.vub.salesmen.entity.Auction;
 import be.vub.salesmen.entity.UserAccount;
+import be.vub.salesmen.entity.UserComment;
 
 import javax.ejb.Local;
 
@@ -11,4 +12,6 @@ public interface ViewAuction
     public void start();
   //  public void selectAuction(Auction a); //deprecated
     public void bid(UserAccount owner);
+	
+	public void addComment(UserAccount user);
 }
