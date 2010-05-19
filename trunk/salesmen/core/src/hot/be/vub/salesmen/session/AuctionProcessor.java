4836a4390ab137fc9f0a839b9cf2b7f909774354
Jorne.laton <Jorne.laton@c5eaa4c8-bb0f-11de-bb27-3175f2afd961>
2010-05-19 09:22:24 +0000

    [code] Solved error in AuctionProcessor

--- trunk/salesmen/core/src/hot/be/vub/salesmen/session/AuctionProcessor.java
+++ trunk/salesmen/core/src/hot/be/vub/salesmen/session/AuctionProcessor.java
@@ -13,24 +13,26 @@ import org.jboss.seam.async.QuartzTriggerHandle;
 import org.jboss.seam.log.Log;
 
 import be.vub.salesmen.entity.Auction;
-import be.vub.salesmen.session.SearchBean;
 
 @Name("processor")
 @AutoCreate
 public class AuctionProcessor {
     
     @In EntityManager entityManager;
+	
+	@In(create=true)
+	EmailService emailService;
 
     @Logger Log log;
 
     @Asynchronous
     @Transactional
     public QuartzTriggerHandle scheduleAuction(@Expiration Date when,
-        @IntervalDuration Long interval, @FinalExpiration Date stoptime, Auction auction) 
+        @IntervalDuration Long interval, @FinalExpiration Date stoptime, Auction auction)
     {
         auction = entityManager.merge(auction);
 		log.info("An auction's deadline has passed.");
-		//new EmailService().sendFinishedAuctionEmail(auction);
+		emailService.sendFinishedAuctionEmail(auction);
 		
         if (auction.getStatus() == Auction.AuctionStatus.LISTED)
         {
